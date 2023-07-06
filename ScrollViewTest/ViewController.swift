751dc9cc66973346025c80049d0db7bfdde50f3f
Phenyo Malele <phenyomalele@Phenyos-MacBook-Air.local>
2023-07-06 20:16:23 +0200

    vertical scroll view implementation

---
+++ ScrollViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewTest
+//
+//  Created by Phenyo Malele on 2023/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
